c1375bef296afff062f1a399e2a34d1ef87431b9
VIJETH U <vijethumesh2002@gmail.com>
2024-04-22 14:46:58 +0530

    Fix pdf

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,22 +6,45 @@ interface SearchParams {
 
 function Page({ searchParams }: { searchParams: SearchParams }) {
   const cardTitle = searchParams.title;
-  let pdf = "";
+  let laundry = false;
+  let food = false;
+  let spa = false;
+
   
   if (cardTitle === "laundry") {
-    pdf = "/RockRegencyLaundry.pdf#toolbar=0";
-  } else if (cardTitle === "food") {
-    pdf = "/RockRegencyFood.pdf#toolbar=0";
+    laundry = true;
+} else if (cardTitle === "food") {
+    food = true;
+    
   } else if (cardTitle === "spa") {
-    pdf = "/RockRegencySpa.pdf#toolbar=0";
+    spa = true;
+    
   }
+
 
   return (
     <div>
-      <iframe
-        src={pdf}
-      />
-    </div>
+    {laundry && (
+        <div className="overflow-hidden mx-auto max-w-full md:max-w-screen-sm">
+        <img src='/assets/RockRegencyLaundry.png' className="w-full h-auto" alt="Rock Regency Laundry"></img>
+      </div>
+    )}
+    {food && (
+        <div className="overflow-hidden mx-auto max-w-full md:max-w-screen-sm">
+        <img src='/assets/RockRegencyFood.png' className="w-full h-auto" alt="Rock Regency Food"></img>
+      </div>
+    )}
+    {spa && (
+        <div className="overflow-hidden mx-auto max-w-full md:max-w-screen-sm">
+        <img src='/assets/RockRegencySpa1.png' className="w-full h-auto" alt="Rock Regency Laundry"></img>
+        <img src='/assets/RockRegencySpa2.png' className="w-full h-auto" alt="Rock Regency Laundry"></img>
+
+      </div>
+    )}
+
+
+</div>
+
   );
 }
 
